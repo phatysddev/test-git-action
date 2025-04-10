8c8c29083882a48d7757e5ea6749943e2416c552
phatysddev <phatysd.dev@gmail.com>
2025-04-10 17:23:45 +0700

    first commit

---
+++ server/src/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => res.json({ message: "hello world" }))
+
+app.listen(3000, () => console.log("server running on port 3000"))
